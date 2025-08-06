0015c752cfb738bb07ba7e6c7131c27d625a22f7
Duy Thai <duy.thai@keytechx.com>
2025-08-06 12:17:33 +0700

    remove tests

--- src/Application/SentraUnitTests/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Threading.Tasks;
-global using Xunit;
-global using FluentAssertions;
-global using Moq;
-global using AutoFixture;
