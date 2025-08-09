736b6bc75c861f5c0cc5406bfabd7a715a1ff02e
Duy Thai <duy.thai@keytechx.com>
2025-08-09 08:52:39 +0700

    Sentra: Add unit tests

---
+++ SentraUnitTests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+// Global using statements based on actual dependencies
+global using System;
+global using System.Threading.Tasks;
+global using Xunit;
+global using FluentAssertions;
+global using Moq;
+global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.Extensions.Configuration;
